fd9b6b994d425e5d8d4358cd0e0cfe9b835304da
Jeroen van der Tuin <jeroen@vandertuin.nl>
2022-03-27 11:12:21 -0600

    Check for active shifts before creating new one.

--- pages/api/shift/start.ts
+++ pages/api/shift/start.ts
@@ -1,17 +1,33 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { ShiftStatus } from ".prisma/client";
 
 import { getSession } from "next-auth/react";
 import { prisma } from "../../../src/prisma";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const session = await getSession({ req });
+  const id = session?.user?.id;
 
-  const userId = session?.user.id;
+  const activeShifts = await prisma.shift.findMany({
+    where: {
+      status: ShiftStatus.ACTIVE,
+    },
+    select: {
+      id: true,
+    },
+  });
 
-  // @todo Check if there is already a shift in progress.
+  if (activeShifts.length !== 0) {
+    res.status(400).json({
+      error: "Can't create a shift when shift is already in progress.",
+      activeShifts,
+    });
+    return;
+  }
 
   const result = await prisma.shift.create({
-    data: { user: { connect: { id: userId } } },
+    data: { user: { connect: { id } } },
   });
+
   res.json(result);
 };
