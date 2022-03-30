506710c7068103737fc18247fd1ba458289c99f3
Jeroen van der Tuin <jeroen@vandertuin.nl>
2022-03-29 21:46:52 -0600

    List dates and shift time in shift list.

--- components/ShiftList/Item.tsx
+++ components/ShiftList/Item.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { timeUnits } from '../../lib/time'
 import { Shift } from '.prisma/client'
 
 interface ShiftListItemProps {
@@ -6,9 +7,32 @@ interface ShiftListItemProps {
 }
 
 const ShiftListItem: React.FC<ShiftListItemProps> = ({ shift }) => {
+  const formatDate = (datetime: Date) => {
+    const date = new Date(datetime);
+    return date.toLocaleDateString();
+  }
+
+  const startDate = formatDate(shift.createdAt);
+  const endDate = formatDate(shift.updatedAt);
+
+  const startTime = new Date(shift.createdAt).getTime();
+  const currentTime = new Date(shift.updatedAt).getTime();
+  const units = timeUnits(currentTime - startTime)
+
   return (
     <li>
-      {shift.createdAt} - {shift.updatedAt} - {shift.status} - {shift.location}
+      <div>{shift.location}</div>
+      <div>{startDate}</div>
+      {startDate !== endDate && (
+        <div>{endDate}</div>
+      )}
+      {units && (
+        <div>
+          {units.hours < 10 ? `0${units.hours}` : units.hours}:
+          {units.minutes < 10 ? `0${units.minutes}` : units.minutes}:
+          {units.seconds < 10 ? `0${units.seconds}` : units.seconds}
+        </div>
+      )}
     </li>
   )
 }
