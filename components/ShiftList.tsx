0fd71b21f1dd2269c5df292349dc87742176d991
Jeroen van der Tuin <jeroen@vandertuin.nl>
2022-03-30 17:31:57 -0600

    Move shiftlist logic to useShiftList.

--- components/ShiftList.tsx
+++ components/ShiftList.tsx
@@ -1,37 +1,14 @@
-import React, { useState, useEffect } from 'react'
-import type { Shift } from '.prisma/client'
+import React from 'react'
 import type { State } from '../pages/index'
+import useShiftList from '../hooks/useShiftList'
 import Item from './ShiftListItem'
-
-type status = 'idle' | 'loading' | 'data' | 'error'
 
 interface Props {
   state: State
 }
 
-const headers = { 'Content-Type': 'application/json' }
-const fetchInit = { method: 'GET', headers }
-
 const ShiftList: React.FC<Props> = ({ state }) => {
-  const [status, setStatus] = useState<status>('idle')
-  const [shifts, setShifts] = useState<Shift[]>([]);
-
-  const fetchShifts = async () => {
-    setStatus('loading');
-    try {
-      const response = await fetch('/api/shift/inactive', fetchInit)
-      const json = await response.json();
-      setStatus('data');
-      setShifts(json.shifts);
-    } catch (error) {
-      setStatus('error')
-    }
-  }
-
-  useEffect(() => {
-    if (state.startedAt !== null) return;
-    fetchShifts();
-  }, [state.startedAt])
+  const { status, shifts } = useShiftList(state)
 
   return (
     <div>
---
+++ hooks/useShiftList.ts
@@ -0,0 +1,38 @@
+import { useState, useEffect } from 'react'
+import type { Shift } from '.prisma/client'
+import type { State } from '../pages/index'
+
+type Status = 'idle' | 'loading' | 'data' | 'error'
+
+const headers = { 'Content-Type': 'application/json' }
+const fetchInit = { method: 'GET', headers }
+
+export default function useShiftList(state: State): {
+  status: Status
+  shifts: Shift[]
+} {
+  const [status, setStatus] = useState<Status>('idle')
+  const [shifts, setShifts] = useState<Shift[]>([])
+
+  const fetchShifts = async () => {
+    setStatus('loading')
+    try {
+      const response = await fetch('/api/shift/inactive', fetchInit)
+      const json = await response.json()
+      setStatus('data')
+      setShifts(json.shifts)
+    } catch (error) {
+      setStatus('error')
+    }
+  }
+
+  useEffect(() => {
+    if (state.startedAt !== null) return
+    fetchShifts()
+  }, [state.startedAt])
+
+  return {
+    status,
+    shifts,
+  }
+}
