bdf695793137f8fe50407c5b55ab3c536f6eebff
Jeroen van der Tuin <jeroen@vandertuin.nl>
2022-03-30 10:55:54 -0600

    Remove check from initial example.

--- lib/time.ts
+++ lib/time.ts
@@ -1,15 +1,12 @@
 /**
  * Converts milliseconds into greater time units as possible
  * @param {int} ms - Amount of time measured in milliseconds
- * @return {Object|null} Reallocated time units. NULL on failure.
+ * @return {Object} Reallocated time units. NULL on failure.
+ * @todo Define return value with Typescript
  * @link  https://stackoverflow.com/a/68673714
  */
 export function timeUnits(ms: number) {
-  if (!Number.isInteger(ms)) {
-    return null;
-  }
-
-  let remainder: number = ms;
+  let remainder: number = ms
 
   /**
    * Takes as many whole units from the time pool (ms) as possible
@@ -17,10 +14,10 @@ export function timeUnits(ms: number) {
    * @return {int} Number of units taken from the time pool
    */
   const allocate = (msUnit: number) => {
-    const units = Math.trunc(remainder / msUnit);
-    remainder -= units * msUnit;
-    return units;
-  };
+    const units = Math.trunc(remainder / msUnit)
+    remainder -= units * msUnit
+    return units
+  }
   // Property order is important here.
   // These arguments are the respective units in ms.
   return {
@@ -30,5 +27,5 @@ export function timeUnits(ms: number) {
     minutes: allocate(60000),
     seconds: allocate(1000),
     // ms: remainder, // remainder
-  };
+  }
 }
