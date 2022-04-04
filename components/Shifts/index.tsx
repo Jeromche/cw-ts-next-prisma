9db06b6dac940dfe0bffa1903ab9c4cdff32d3dd
Jeroen van der Tuin <jeroen@vandertuin.nl>
2022-04-04 15:13:53 -0500

    Move shiftProvider to Shifts component.

--- components/Shifts/index.tsx
+++ components/Shifts/index.tsx
@@ -1,12 +1,15 @@
 import Timer from '../../components/Timer';
 import ShiftList from '../../components/ShiftList';
+import { ShiftProvider } from "../../store";
 import styles from './Shifts.module.css'
 
 const Shifts: React.FC = () => (
-  <div className={styles.container}>
-    <Timer />
-    <ShiftList />
-  </div>
+  <ShiftProvider>
+    <div className={styles.container}>
+      <Timer />
+      <ShiftList />
+    </div>
+  </ShiftProvider>
 )
 
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,12 @@
 import type { AppProps } from 'next/app'
 import { SessionProvider } from "next-auth/react"
-import { ShiftProvider } from "../store";
 
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
-      <ShiftProvider>
-        <Component {...pageProps} />
-      </ShiftProvider>
+      <Component {...pageProps} />
     </SessionProvider>
   )
 }
